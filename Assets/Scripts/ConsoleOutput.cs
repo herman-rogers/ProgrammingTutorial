f9940eedb08f1cf7ae1dedeb25610ebcc31a7534
Boomer <herman.rogers@gmail.com>
2015-10-09 21:36:26 +0100

    Adding initial files

---
+++ Assets/Scripts/ConsoleOutput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsoleOutput : MonoBehaviour {
+
+
+    int myWord = 1;
+    int numberTwo = 0;
+
+    void Start( ) {
+        test( );
+    }
+    
+    void test( ) {
+        myWord = myWord + myWord;
+        Debug.Log( myWord );
+    }
+
+}
+
+public class NewClass : MonoBehaviour { 
+
+}
